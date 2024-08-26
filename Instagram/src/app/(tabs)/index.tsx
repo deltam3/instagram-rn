2b7db05793ce185140aec7bfc501083a43b715f4
deltam3 <practicect@proton.me>
2024-08-26 19:45:39 +0900

    basic post page layout

--- Instagram/src/app/(tabs)/index.tsx
+++ Instagram/src/app/(tabs)/index.tsx
@@ -1,12 +1,18 @@
 import { FlatList } from "react-native";
 import posts from "../../../assets/data/posts.json";
 import PostListItem from "~/src/components/PostListItem";
+import { styled } from "nativewind";
 
 export default function FeedScreen() {
   return (
     <FlatList
       data={posts}
-      contentContainerStyle={{ gap: 10 }}
+      contentContainerStyle={{
+        gap: 10,
+        maxWidth: 512,
+        width: "100%",
+        alignSelf: "center",
+      }}
       showsVerticalScrollIndicator={false}
       renderItem={({ item }) => <PostListItem post={item} />}
     />
