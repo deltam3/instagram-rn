9dd4f2edabf346ff3e99bf090332916afca45317
deltam3 <practicect@proton.me>
2024-08-31 14:39:53 +0900

    basic supabase setup and auth template files

--- Instagram/src/app/index.tsx
+++ Instagram/src/app/index.tsx
@@ -1,7 +1,7 @@
 import { Redirect } from "expo-router";
-import { Text } from "react-native";
 
 export default function Home() {
-  return <Redirect href="/(tabs)" />;
+  return <Redirect href="/(auth)" />;
+  // return <Redirect href="/(tabs)" />;
   // return <Text>Hello</Text>;
 }
