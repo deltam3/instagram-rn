6dd44e1fa4da5af54a6b04d135984f0c7b3e3ff4
deltam3 <practicect@proton.me>
2024-09-01 09:33:42 +0900

    update profile form functionality and created customtextinput component

--- Instagram/src/app/(tabs)/profile.tsx
+++ Instagram/src/app/(tabs)/profile.tsx
@@ -1,18 +1,65 @@
-import { Text, View, Image, TextInput } from "react-native";
+import { Text, View, Image, TextInput, Alert } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import { useState, useEffect } from "react";
 import Button from "~/src/components/Button";
 import { supabase } from "~/src/lib/supabase";
+import { useAuth } from "~/src/providers/AuthProvider";
+import CustomTextInput from "~/src/components/CustomTextInput";
 
 export default function ProfileScreen() {
   const [image, setImage] = useState<string | null>(null);
   const [username, setUsername] = useState("");
+  const [bio, setBio] = useState("");
 
-  // useEffect(() => {
-  //   if (!image) {
-  //     pickImage();
-  //   }
-  // }, [image]);
+  const { user } = useAuth();
+
+  useEffect(() => {
+    getProfile();
+  }, []);
+
+  const getProfile = async () => {
+    if (!user) {
+      return;
+    }
+    const { data, error } = await supabase
+      .from("profiles")
+      .select("*")
+      .eq("id", user?.id)
+      .single();
+
+    if (error) {
+      Alert.alert("Failed to fetch profile");
+    }
+
+    setUsername(data.username);
+    setBio(data.bio);
+    // setRemoteImage(data.avatar_url);
+  };
+
+  const updateProfile = async () => {
+    if (!user) {
+      return;
+    }
+
+    const updatedProfile = {
+      id: user.id,
+      username,
+      bio,
+    };
+
+    if (image) {
+      const response = await uploadImage(image);
+      updatedProfile.avatar_url = response.public_id;
+    }
+
+    const { data, error } = await supabase
+      .from("profiles")
+      .update(updatedProfile);
+
+    if (error) {
+      Alert.alert("Failed to update profile");
+    }
+  };
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -48,16 +95,26 @@ export default function ProfileScreen() {
         Change
       </Text>
       {/* Form */}
-      <Text className="mb-2 text-gray-700 font-semibold">Username</Text>
-      <TextInput
-        placeholder="Username"
-        value={username}
-        onChangeText={setUsername}
-        className="border border-gray-300 p-3 rounded-md"
-      />
+      <View className="gap-5">
+        <CustomTextInput
+          label="Username"
+          placeholder="Username"
+          value={username}
+          onChangeText={setUsername}
+        />
+
+        <CustomTextInput
+          label="Bio"
+          placeholder="Bio"
+          value={bio}
+          onChangeText={setBio}
+          multiline
+          numberOfLines={3}
+        />
+      </View>
       <View className="gap-2 mt-auto">
-        <Button title="Update profile" />
-        {/* <Button title="Sign out" /> */}
+        {/* <Button title="Update profile" /> */}
+        <Button title="Update profile" onPress={updateProfile} />
         <Button title="Sign out" onPress={() => supabase.auth.signOut()} />
       </View>
 
